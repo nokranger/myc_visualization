bf8ef62c108cf0465b06c6595b0c198894b01bdc
nokranger <anusorn.thavornporn@gmail.com>
2020-05-22 16:40:30 +0700

    page setting

--- src/views/Setting.vue
+++ src/views/Setting.vue
@@ -1,11 +1,65 @@
 <template>
   <div>
     Setting
+  <b-container>
+    <b-row>
+      <b-col>
+        <div>
+          <div style="text-align:right">
+            Monthly
+          </div>
+        </div>
+      </b-col>
+      <b-col>
+        <div style="text-align:left">
+          <b-form-select id="month" v-model="selected" :options="options"></b-form-select>
+        </div>
+      </b-col>
+      <b-col></b-col>
+    </b-row>
+    <b-row>
+      <b-col>
+        <div style="text-align:right">
+          Saler Name
+        </div>
+      </b-col>
+      <b-col>
+        <div style="text-align:left">
+          <b-form-select id="month" v-model="selected" :options="options"></b-form-select>
+        </div>
+      </b-col>
+      <b-col></b-col>
+    </b-row>
+    <b-row>
+      <b-col style="text-align:right">
+        <div>
+          Product
+        </div>
+      </b-col>
+      <b-col style="text-align:left">
+        <div>
+          <b-form-select id="month" v-model="selected" :options="options"></b-form-select>
+        </div>
+      </b-col>
+      <b-col></b-col>
+    </b-row>
+  </b-container>
   </div>
 </template>
 <script>
 export default {
   data () {
+    return {
+      selected: null,
+      options: [
+        {
+          value: null, text: 'Jan'
+        },
+        {
+          value: 1, text: 'Fab'
+        }
+      ]
+    }
   },
   metaInfo () {
     return {
